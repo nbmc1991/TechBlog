5157b888974f5efe5770d863fd29b0d845ab08c1
nbmc1991 <nbmc1991@gmail.com>
2020-12-23 22:23:54 -0500

    connection

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const Sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+    }
+
+  );
+
+module.exports = sequelize;
