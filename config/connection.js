ce5c0b334e13d7fa72ed050f4e9b36e8ad6ed208
nbmc1991 <nbmc1991@gmail.com>
2020-12-26 15:33:29 -0500

    modifuing connection

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,20 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: process.env.DB_HOST,
+      host: 'localhost',
       dialect: 'mysql',
+      port: 7777
     }
-
   );
-
+}
 module.exports = sequelize;
