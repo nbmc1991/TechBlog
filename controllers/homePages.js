d7024cc1e520c1741cdcd177d8b7ffe628b5b3c7
nbmc1991 <nbmc1991@gmail.com>
2020-12-30 17:53:06 -0500

    setting up homepages

--- controllers/homePages.js
+++ controllers/homePages.js
@@ -1,2 +1,53 @@
 const router = require('express').Router();
-const { User, Blog, Comment } = require('.././models')
+const { User, Blog, Comment } = require('.././models');
+// const withAuth =require('../utils/auth');
+
+// GET ALL BLOGS FOR HOMEPAGE
+router.get('/', async (req, res) => {
+    try {
+        const dbBlogData = await Blog.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+        const blogs = dbBlogData.map((blog) =>
+            blog.get({ plain: true })
+        );
+        res.render('homepage', {
+            blogs,
+            // loggedIn: req.session.loggedIn,
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// //GET ONE BLOG
+router.get('/blog/:id', async (req, res) => {
+    try {
+        const dbBlogData = await Blog.findByPk(req.params.id, {
+            indlude: [
+                {
+                    model: Comment,
+                    attributes: [
+                        'comment',
+                        'user_id',
+
+                    ],
+                },
+            ],
+        }),
+        const blog = dbBlogData.get({ plain: true });
+        res.render('blog', { blog });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+
--- controllers/index.js
+++ controllers/index.js
@@ -1,2 +1,13 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+
+// const homePages = require('./homePages');
+
+// router.use('/', homePages);
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.send("<h1>Change PATH!</h1>")
+});
+
+module.exports = router;
