39d0b477867480bb1090ccc1ee809e42e42a31b9
nbmc1991 <nbmc1991@gmail.com>
2021-01-05 00:37:49 -0500

    update

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,14 +1,15 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
 
-    const name = document.querySelector('#blog-name').value.trim();
-    const needed_funding = document.querySelector('#blog-funding').value.trim();
-    const description = document.querySelector('#blog-content').value.trim();
+    const title = document.querySelector('#blog-name').value.trim();
+    const comment = document.querySelector('#blog-funding').value.trim();
+    const content = document.querySelector('#blog-content').value.trim();
 
-    if (name && needed_funding && description) {
+    if (title && comment && content) {
+
         const response = await fetch(`/api/blogs`, {
             method: 'POST',
-            body: JSON.stringify({ name, needed_funding, description }),
+            body: JSON.stringify({ title, comment, content }),
             headers: {
                 'Content-Type': 'application/json',
             },
